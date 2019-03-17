da53011b92b99a10bebd1f371832494901ed1b03
Muhammad Taufiq Hidayat <taufiq28101998@gmail.com>
2019-03-17 12:30:15 +0700

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+/**
+ * using schema
+ * Index Lengths & MySQL / MariaDB
+ */
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        Schema::defaultStringLength(191);
+    }
+}
