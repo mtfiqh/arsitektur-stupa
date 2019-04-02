b7a1a6033e35d711ffd20a67d02cfad644224fae
dzakydr <dzaky.14116041@student.itera.ac.id>
2019-04-02 12:20:05 +0700

    dashboard admin

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -1,44 +1,52 @@
 @extends('voyager::master')
 
 @section('content')
-<div class="container">
-    
-      <div class="row mb-5">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
-        <div class="col-md">
-            <div class="card"style="18rem;">
-                <img class="card-img-top rounded-circle img-thumbnail" src="gambar/ec.jpg" >
-                    <div class="card-body"><h3>ENGLISH CLUB</h3>
-                      <p class="card-text">English club adalah sebuah lembaga, tempat berkumpul dan belajar bersama untuk mengerti dan memahami cara berbicara dalam bahasa inggris. English Club SMA Tentang Rindu dibentuk pada tahun 2010. </p>
-                      <a href="EC.html" class="card-link">Read More</a>
-                    </div>
-                  </div>
+<section id="ekskul" class="ekskul bg-light pb-5">
+      <div class="container-fluid">
+            <div class="row mb-4 mt-4 pt-4">
+                <div class="col text-center">
+                <h1> Dashboard Admin </h1>
+                </div>
+            </div>
+
+
+        <div class="row mb-5" style="margin-top:10px">
+
+            <div class="col-md-4">
+                    <div class="card" style="16rem;">
+                        <img class="card-img-top rounded-circle img-thumbnail" src="gambar/user2.png" alt="Card image" style="width:100%">
+                            <div class="card-body"><h3></h3>
+                              <p class="card-text" > </p>
+                              <a href="Musik.html" class="card-link" style="font-size:30px;">User</a>
+                            </div>
+                          </div>
+            </div>
+
+            <div class="col-md-4">
+                    <div class="card"style="18rem;">
+                        <img class="card-img-top rounded-circle img-thumbnail" src="gambar/kelas.png" style="width:100%" >
+                            <div class="card-body"><h3></h3>
+                              <p class="card-text"></p>
+                              <a href="Pramuka.html" class="card-link"  style="font-size:30px;">Kelas</a>
+                            </div>
+                          </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="card"style="18rem;">
+                    <img class="card-img-top rounded-circle img-thumbnail" src="gambar/futsal.jpg" style="width:100%">
+                        <div class="card-body"><h3>FUTSAL</h3>
+                          <p class="card-text-center">Tim Futsal yang telah berdiri sejak tahun 2010 ini sering memiliki nama tim Penta FC. Penta FC dilatih oleh Willy yang mana menangani tim ini sejak tim ini dibentuk.</p>
+                          <a href="Futsal.html" class="card-link">Read More</a>
+                        </div>
+                      </div>
+            </div>
         </div>
 
-        <div class="col-md">
-          <div class="card"style="18rem;">
-              <img class="card-img-top rounded-circle img-thumbnail" src="gambar/silat.jpg" >
-                  <div class="card-body"><h3>PENCAK SILAT</h3>
-                    <p class="card-text">Ekstrakulikuler Pencak Silat yang telah berdiri sejak 2017 yang dilatih oleh Kevin yang mana menangani sejak ekskul ini dibentuk.</p>
-                    <a href="Silat.html" class="card-link">Read More</a>
-                  </div>
-                </div>
-      </div>
-
-      <div class="col-md">
-          <div class="card"style="18rem;">
-              <img class="card-img-top rounded-circle img-thumbnail" src="{{asset('gambar/basket.jpg')}}" >
-                  <div class="card-body"><h3>BASKET</h3>
-                    <p class="card-text">Tim basket SMA Tentang Rindu sudah ada sejak tahun 2001. Tim Basket SMA Tentang Rindu dilatih oleh Bobby untuk tim putra dan Tanti untuk tim putri.</p>
-                    <a href="Basket.html" class="card-link">Read More</a>
-                  </div>
-                </div>
-      </div>
-  </div>
         </div>
-      </div>
-    </div>
-    </div>
+      </section>
 @stop
 
 
