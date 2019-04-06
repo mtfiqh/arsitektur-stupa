1799b3ecc11a34d309e232e062c52effa1cd4acd
Riadi123 <44835552+Riadi123@users.noreply.github.com>
2019-04-07 02:20:06 +0700

    Update dosen.blade.php

--- resources/views/dashboard/dosen.blade.php
+++ resources/views/dashboard/dosen.blade.php
@@ -3,3 +3,33 @@
 @section('content')
 
 @endsection
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>STUPA ITERA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      media="screen"
+      href="css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <img
+      src="img/gg.png"
+      width="32%"
+      style="margin-left: 17.5%; margin-top: 30px"
+    />
+    <img
+      src="img/jj.png"
+      width="32%"
+      style="margin-left: 10px; margin-top: 30px"
+    />
+  </body>
+</html>
