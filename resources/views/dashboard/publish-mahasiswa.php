0eee692e44933d2b063b05ca88235849486a0ad8
Riadi123 <44835552+Riadi123@users.noreply.github.com>
2019-05-07 07:42:29 +0700

    Publish-Mahasiswa
    Hari ini aku pengen bobo panjang

---
+++ resources/views/dashboard/publish-mahasiswa.php
@@ -0,0 +1,189 @@
+@extends('voyager::master')
+
+@section('content')
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+  @import "https://fonts.googleapis.com/css?family=Poppins:600";
+  body{
+    background-color: #111111;
+  }
+* {
+  box-sizing: border-box;
+}
+h2{
+  font-family: "Poppins", sans-serif;
+  font-size: 50px;
+  color: #F3DA32;
+  text-shadow: 2px 2px white;
+  text-align: center;
+}
+/* Create three equal columns that floats next to each other */
+.column {
+  width: 225px;
+  height: 296.25px;
+  margin: 5px;
+  margin-left: 250px;/* Should be removed. Only for demonstration */
+}
+.column2 {
+  width: 375px;
+  height: 195px;
+  margin: 5px;/* Should be removed. Only for demonstration */
+}
+.column3 {
+  width: 225px;
+  height: 296.25px;
+  margin: 5px;/* Should be removed. Only for demonstration */
+/* Clear floats after the columns */}
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+.w3-card-4{
+  cursor: pointer;
+  background-color: #F3DA32;
+  border-radius: 5%;
+}
+.w3-card-4:hover{
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+p{
+  font-family: "Poppins", sans-serif;
+  color: black;
+}
+</style>
+</head>
+<body>
+
+<h2>Galeri Stupa Arsitektur Itera</h2>
+
+<div class="row">
+  <div class="row" style="float: left;">
+    <div class="column">
+      <div class="w3-card-4" style="width:225px; height: 296.25px;">
+       
+        <div class="w3-container w3-center">
+          <p>The Italian / Austrian Alps</p>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+        <div class="w3-card-4" style="width:225px; height: 296.25px;">
+        
+            <div class="w3-container w3-center">
+              <p>The Italian / Austrian Alps</p>
+            </div>
+          </div>
+    </div>
+    </div>
+  <div class="row" style="float: left;">
+   <div class="column2">
+      <div class="w3-card-4" style="width:375px; height: 195px;">
+      
+          <div class="w3-container w3-center">
+            <p>The Italian / Austrian Alps</p>
+          </div>
+        </div>
+  </div>
+  <div class="column2">
+      <div class="w3-card-4" style="width:375px; height: 195px;">
+     
+          <div class="w3-container w3-center">
+            <p>The Italian / Austrian Alps</p>
+          </div>
+        </div>
+  </div>
+  <div class="column2">
+      <div class="w3-card-4" style="width:375px; height: 195px;">
+     
+          <div class="w3-container w3-center">
+            <p>The Italian / Austrian Alps</p>
+          </div>
+        </div>
+  </div>
+  </div>
+  <div class="row" style="float: left;">
+  <div class="column3">
+      <div class="w3-card-4" style="width:225px; height: 296.25px;">
+        
+          <div class="w3-container w3-center">
+            <p>The Italian / Austrian Alps</p>
+          </div>
+        </div>
+  </div>
+  <div class="column3">
+      <div class="w3-card-4" style="width:225px; height: 296.25px;">
+         
+          <div class="w3-container w3-center">
+            <p>The Italian / Austrian Alps</p>
+          </div>
+        </div>
+  </div>
+  </div>
+  <div class="row" style="float: left;">
+    <div class="column2">
+        <div class="w3-card-4" style="width:375px; height: 195px;">
+         
+            <div class="w3-container w3-center">
+              <p>The Italian / Austrian Alps</p>
+            </div>
+          </div>
+    </div>
+    <div class="column2">
+        <div class="w3-card-4" style="width:375px; height: 195px;">
+           
+            <div class="w3-container w3-center">
+              <p>The Italian / Austrian Alps</p>
+            </div>
+          </div>
+    </div>
+    <div class="column2">
+        <div class="w3-card-4" style="width:375px; height: 195px;">
+       
+            <div class="w3-container w3-center">
+              <p>The Italian / Austrian Alps</p>
+            </div>
+          </div>
+    </div>
+    </div>
+</div>
+</div>
+
+
+
+</body>
+
+@endsection
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>STUPA ITERA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      media="screen"
+      href="css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <img
+      src="img/gg.png"
+      width="32%"
+      style="margin-left: 17.5%; margin-top: 30px"
+    />
+    <img
+      src="img/jj.png"
+      width="32%"
+      style="margin-left: 10px; margin-top: 30px"
+    />
+  </body>
+</html>
